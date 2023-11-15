a992f83721f7dc31f95d93fc9f61a0344642a219
EwenBernard <ewen.bernard@efrei.net>
2023-11-15 12:01:30 +0100

    Added weather data process + change main with json

--- main.py
+++ main.py
@@ -2,26 +2,6 @@ import pygame
 import sys
 import math
 
-# Initialize Pygame
-pygame.init()
-
-# Constants
-WIDTH, HEIGHT = 300, 200
-BG_COLOR = (0, 0, 0)
-RAY_COLOR = (255, 255, 255)
-
-# Set up the screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("ASCII Ray Tracing")
-
-# Ray-tracing function
-def ray_trace(origin, direction, t_min, t_max):
-    for t in range(t_min, t_max):
-        x, y = origin[0] + int(direction[0] * t), origin[1] + int(direction[1] * t)
-        pygame.draw.circle(screen, RAY_COLOR, (x, y), 1)
-        pygame.display.flip()
-
-# Main loop
 def main():
     while True:
         for event in pygame.event.get():
